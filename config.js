cec1e3ee73be20355e1e211b9046365dbb792971
aaru123 <72920365+aaru123@users.noreply.github.com>
2021-09-02 15:49:30 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig={
+    apiKey: "AIzaSyDd_xNYCPKrO6wKtTCt63_qL5gputsNE3c",
+        authDomain: "storytelling-app-a08a8.firebaseapp.com",
+        projectId: "storytelling-app-a08a8",
+        storageBucket: "storytelling-app-a08a8.appspot.com",
+        messagingSenderId: "550604211086",
+        appId: "1:550604211086:web:bb906b0524a9ddd455dadf"
+      };
+      
